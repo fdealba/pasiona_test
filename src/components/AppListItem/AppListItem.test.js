a14238f7562e9ebd5f3a931d84670b19461660ee
Felipe de Alba <felipe.de.alba.alegre@gmail.com>
2021-04-08 12:44:17 +0200

    minor fix

--- src/components/AppListItem/AppListItem.test.js
+++ src/components/AppListItem/AppListItem.test.js
@@ -7,10 +7,15 @@ describe('App list item', () => {
 
     const appListItem = screen.getByTestId('appListItem');
     const controls = screen.getByTestId('controls');
+    const editIcon = screen.getByTestId('edit');
+    const deleteIcon = screen.getByTestId('delete');
+
     const content = screen.getByText(/list item/i);
     
     expect(appListItem).toBeInTheDocument();
     expect(controls).toBeInTheDocument();
     expect(content).toBeInTheDocument();
+    expect(editIcon).toBeInTheDocument();
+    expect(deleteIcon).toBeInTheDocument();
   });
 })
