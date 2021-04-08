67df9652e4f6b6c0f314b90ae21d347bdfc188db
Felipe de Alba <felipe.de.alba.alegre@gmail.com>
2021-04-08 11:36:20 +0200

    app-editable-list done, missing tests

--- src/components/AppEditableList/AppEditableList.js
+++ src/components/AppEditableList/AppEditableList.js
@@ -1,27 +1,43 @@
 // Styles
 import "./AppEditableList.css";
-import AppListItem from '../AppListItem/AppListItem';
-import { useState, useRef } from "react";
+import AppListItem from "../AppListItem/AppListItem";
+import { useState, useRef, useEffect } from "react";
 
 const AppEditableList = () => {
   const addItemInput = useRef(null);
+  const [items, setItems] = useState([]);
 
-  const addItemHandler = (event) => {
-    console.log(addItemInput.current.value);
+  // Clean input field after adding an item
+  useEffect(() => {
+    addItemInput.current.value = "";
+  }, [items])
+
+  const onAddItemHandler = () => {
+    if (addItemInput.current.value) {
+      setItems((previousItems) => [...previousItems, addItemInput.current.value]);
+    }
+  };
+
+  const onKeyDownHandler = (event) => {
+    if (event.keyCode === 13) {
+      onAddItemHandler()
+    }
   }
 
+  const listItems = items.map((item, idx) => (
+    <AppListItem key={idx} content={item} itemIdx={idx} setItems={setItems} />
+  ));
+
   return (
-  <div className="app-editable-list">
-    <div className="new-item-container">
-      <input type="text" ref={addItemInput}/>
-      <div className="submit" onClick={addItemHandler}>Add item</div>
+    <div className="app-editable-list">
+      <div className="new-item-container">
+        <input type="text" onKeyDown={onKeyDownHandler} ref={addItemInput} />
+        <div className="submit" onClick={onAddItemHandler}>
+          Add item
+        </div>
+      </div>
+      {listItems}
     </div>
-    <AppListItem content="hi i am app list item"/>
-    <AppListItem content="hi i am app list item"/>
-    <AppListItem content="hi i am app list item"/>
-    <AppListItem content="hi i am app list item"/>
-    <AppListItem content="hi i am app list item"/>
-  </div>
   );
 };
 
--- src/components/AppListItem/AppListItem.js
+++ src/components/AppListItem/AppListItem.js
@@ -1,20 +1,65 @@
-import './AppListItem.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { useState, useRef } from "react";
+import "./AppListItem.css";
 
-const AppListItem = ({ content }) => {
+// Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+const AppListItem = ({ content, itemIdx, setItems }) => {
+  const [editMode, setEditMode] = useState(false);
+  const editItemInput = useRef(null);
+
+  const onEditClickedHandler = () => {
+    setEditMode(true);
+  };
+
+  const onKeyDownHandler = (event) => {
+    if (event.keyCode === 13) {
+      handleEditModeEnd();
+    }
+  };
+
+  const handleEditModeEnd = () => {
+    setItems((previousItems) => {
+      const newItems = [...previousItems];
+      newItems[itemIdx] = editItemInput.current.value;
+      return newItems;
+    });
+    setEditMode(false);
+  };
+
+  const onDeleteClickedHandler = () => {
+    setItems((previousItems) =>
+      previousItems.filter((item, idx) => idx !== itemIdx)
+    );
+  };
+
   return (
     <div className="app-list-item">
       <div className="content-container">
-        { content }
+        {editMode ? (
+          <input
+            autoFocus
+            onKeyDown={onKeyDownHandler}
+            onBlur={handleEditModeEnd}
+            defaultValue={content}
+            ref={editItemInput}
+          />
+        ) : (
+          content
+        )}
       </div>
 
       <div className="controls">
-        <a><FontAwesomeIcon icon={faEdit} /></a>
-        <a><FontAwesomeIcon icon={faTrash} /></a>
+        <a>
+          <FontAwesomeIcon icon={faEdit} onClick={onEditClickedHandler} />
+        </a>
+        <a>
+          <FontAwesomeIcon icon={faTrash} onClick={onDeleteClickedHandler} />
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default AppListItem;
